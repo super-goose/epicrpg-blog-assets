20684dc80b9744d54ae16d1c9223f2bbe6c6cc7e
Jay <theworldneedssupergoose@gmail.com>
2018-01-08 13:34:05 -0600

    for blog article

---
+++ 010-CanvasRendering/legend.js
@@ -0,0 +1,11 @@
+var legend = {
+    // ...
+    // other sprites went here
+    // ... ,
+    "zombie-red": {
+        "x": 0,
+        "y": 6560,
+        "width": 32,
+        "height": 32
+    }
+}
